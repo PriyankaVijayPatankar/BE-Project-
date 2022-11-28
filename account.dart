06217b1cf872dc81a2e761368775db75eb2cd682
PriyankaVijayPatankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-11-28 21:01:12 +0530

    Updated by Priyanka
    Minimum code required is uploaded

---
+++ account.dart
@@ -0,0 +1,19 @@
+// this is the minimum code required
+
+import 'package:flutter/material.dart';
+
+class Account extends StatefulWidget {
+  const Account({Key? key}) : super(key: key);
+
+  @override
+  State<Account> createState() => _AccountState();
+}
+
+class _AccountState extends State<Account> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
