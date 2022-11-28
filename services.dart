28ccf19b13b11122d25427e28e08d2f55fe7498a
PriyankaVijayPatankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-11-28 21:05:00 +0530

    Uploaded by Priyanka
    This is the minimum code required

---
+++ services.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Services extends StatefulWidget {
+  const Services({Key? key}) : super(key: key);
+
+  @override
+  State<Services> createState() => _AccountState();
+}
+
+class _AccountState extends State<Services> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
