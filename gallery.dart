1e545f7b54a9bfaac89560464f3dc47486845155
Priyanka Patankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2023-01-27 11:46:17 +0530

    Update gallery.dart

--- gallery.dart
+++ gallery.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/main.dart';
+import 'homepage.dart';
 
 class Gallery extends StatefulWidget {
   const Gallery({Key? key}) : super(key: key);
@@ -10,10 +12,43 @@ class Gallery extends StatefulWidget {
 class _GalleryState extends State<Gallery> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    List<String> images = [
+      "assets/images/main 11.jpeg",
+      "assets/images/main 22.jpeg",
+      "assets/images/main 13.jpeg",
+      "assets/images/main 26.jpeg",
+      "assets/images/main 27.jpeg",
+      "assets/images/main.jpeg",
+      "assets/images/neelam.jpeg",
+      "assets/images/ratna.jpeg",
+      "assets/images/sindhu.jpeg",
+      "assets/images/suvarna.jpeg",
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        title: Text("Gallery"),
+        title: const Text("Gallery", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        leading: BackButton(
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
-    );
+      body: Center(
+          child: GridView.builder(
+        itemCount: images.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 5.0,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Image.asset(images[index]);
+        },
+      )),
+    ));
   }
 }
