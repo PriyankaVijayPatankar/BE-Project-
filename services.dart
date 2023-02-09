4b268424e263478cb9756fa3e5cac5e50332917b
ArpitaVK-16 <87219722+ArpitaVK-16@users.noreply.github.com>
2023-02-09 22:01:53 +0530

    Updated by Arpita

--- services.dart
+++ services.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mymobileapp/main.dart';
+import 'homepage.dart';
 
 class Services extends StatefulWidget {
   const Services({Key? key}) : super(key: key);
@@ -10,6 +12,75 @@ class Services extends StatefulWidget {
 class _AccountState extends State<Services> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Services", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+        leading: BackButton(
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => HomePage()));
+          },
+        ),
+      ),
+      body: Center(
+          child: Column(children: [
+        Card(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset('assets/images/compound_1.jpg'),
+                title: const Text('Booking'),
+                subtitle: const Text('This is a simple card in Flutter.'),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset('assets/images/compound_1.jpg'),
+                title: const Text('Room Services'),
+                subtitle: const Text('This is a simple card in Flutter.'),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset('assets/images/compound_1.jpg'),
+                title: const Text('Sightseeing'),
+                subtitle: const Text('This is a simple card in Flutter.'),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset('assets/images/compound_1.jpg'),
+                title: const Text('Gallery'),
+                subtitle: const Text('This is a simple card in Flutter.'),
+              ),
+            ],
+          ),
+        ),
+      ])),
+    );
   }
 }
