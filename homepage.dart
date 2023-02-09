c82dbc90188d51f4b2d3db19ed30065ba8688812
ArpitaVK-16 <87219722+ArpitaVK-16@users.noreply.github.com>
2023-02-09 21:59:47 +0530

    Updated by Arpita

--- homepage.dart
+++ homepage.dart
@@ -14,87 +14,103 @@ class _HomeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            body: Center(
-                child: Builder(
-                    builder: (context) => SingleChildScrollView(
-                          child: Center(
-                            child: Column(children: [
-                              // make the image size large, occupying the whole width of screen, and atleast 40% of vertical screen
-                              Image.asset('assets/images/compound_1.jpg'),
-                              //Image.network(
-                              //  'https://i1.wp.com/famt.ac.in/wp-content/uploads/2015/08/DSC_0050.jpg',
-                              //  height: 300,
-                              //),
-                              Padding(
-                                  padding: EdgeInsets.all(12.0),
-                                  child: Center(
+      home: Scaffold(
+          body: Center(
+              child: Builder(
+                  builder: (context) => SingleChildScrollView(
+                        child: Center(
+                          child: Column(children: [
+                            // make the image size large, occupying the whole width of screen, and atleast 40% of vertical screen
+                            Image.asset('assets/images/compound_1.jpg',
+                                fit: BoxFit.fill),
+                            //Image.network(
+                            //  'https://i1.wp.com/famt.ac.in/wp-content/uploads/2015/08/DSC_0050.jpg',
+                            //  height: 300,
+                            //),
+                            Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Container(
                                     child: RichText(
-                                      // need to update the font size and indentation
-                                      text: TextSpan(
-                                          text: 'Welcome to\n',
-                                          children: <InlineSpan>[
-                                            TextSpan(
-                                                text: 'Amrapali Gramsahavas\n',
-                                                style: TextStyle(
-                                                    color: Colors.blueAccent,
-                                                    fontSize: 20,
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                            TextSpan(
+                                      text: const TextSpan(
+                                        text: 'Welcome to\n',
+                                        //textAlign: TextAlign.center,
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                              text: 'Amrapali\nGramsahavas\n',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.blueAccent,
+                                                  fontSize: 40)),
+                                          TextSpan(
                                               text:
-                                                  // need to confirm from the client
-                                                  'A fine union of Literature, Environment and Tourism',
-                                            )
-                                          ]),
+                                                  'A fine union of Literature, Environment and Tourism'),
+                                        ],
+                                      ),
                                     ),
-                                  )),
-                              SizedBox(
-                                height: 40,
-                              ),
-                              Column(
-                                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  ButtonTheme(
-                                    minWidth: 400.0,
-                                    height: 60,
-                                    child: RaisedButton(
-                                      child: Text('About Amrapali Gramsahavas',
-                                          style: TextStyle(fontSize: 20)),
-                                      color: Colors.black,
-                                      textColor: Colors.white,
-                                      onPressed: () {
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) => About()));
-                                      },
+                                    alignment: Alignment.center)),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            Column(
+                              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                ButtonTheme(
+                                  minWidth: 400.0,
+                                  height: 60,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.black,
+                                      foregroundColor: Colors.white,
+                                      //elevation: 100,
+                                      padding:
+                                          EdgeInsets.all(20.0), // Elevation
+                                      // shadowColor:
+                                      //     Color.fromARGB(255, 50, 49, 49),
                                     ),
+                                    child: Text(
+                                        'About Amrapali Gramsahavas      >',
+                                        style: TextStyle(fontSize: 20)),
+                                    //color: Colors.black,
+                                    //textColor: Colors.white,
+                                    onPressed: () {
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) => About()));
+                                    },
                                   ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  ButtonTheme(
-                                    minWidth: 400.0,
-                                    height: 60,
-                                    child: RaisedButton(
-                                      child: Text('Check Services',
-                                          style: TextStyle(
-                                              //fontWeight: FontWeight.bold,
-                                              fontSize: 20)),
-                                      color: Colors.black,
-                                      textColor: Colors.white,
-                                      onPressed: () {
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    Services()));
-                                      },
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                ButtonTheme(
+                                  minWidth: 400.0,
+                                  height: 60,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.black,
+                                      foregroundColor: Colors.white,
+                                      padding: EdgeInsets.all(20.0),
                                     ),
+                                    child: Text('Check Services     >',
+                                        style: TextStyle(
+                                            //fontWeight: FontWeight.bold,
+                                            fontSize: 20)),
+                                    //color: Colors.black,
+                                    //textColor: Colors.white,
+                                    onPressed: () {
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  Services()));
+                                    },
                                   ),
-                                ],
-                              ),
-                            ]),
-                          ),
-                        )))));
+                                ),
+                              ],
+                            ),
+                          ]),
+                        ),
+                      )))),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
