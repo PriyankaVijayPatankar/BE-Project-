9b63d72d3d1c8af05d3979c47cc4004462476ce6
ArpitaVK-16 <87219722+ArpitaVK-16@users.noreply.github.com>
2023-02-09 22:00:39 +0530

    Updated by Arpita

--- maps.dart
+++ maps.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mymobileapp/main.dart';
+import 'homepage.dart';
 
 class Maps extends StatefulWidget {
   const Maps({Key? key}) : super(key: key);
@@ -10,6 +12,27 @@ class Maps extends StatefulWidget {
 class _MapsState extends State<Maps> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Maps", style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0,
+          leading: BackButton(
+            color: Colors.black,
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => MyApp()));
+            },
+          ),
+        ),
+        body: Container(
+          height: 400,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/maps.jpg"), fit: BoxFit.cover),
+          ),
+        ));
   }
 }
