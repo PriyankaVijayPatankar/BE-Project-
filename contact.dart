f5fb37f3b5d7b4f23ca60dab21c7b4afcb358a19
ArpitaVK-16 <87219722+ArpitaVK-16@users.noreply.github.com>
2023-02-09 21:58:35 +0530

    Updated  by Arpita

--- contact.dart
+++ contact.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mymobileapp/main.dart';
+import 'homepage.dart';
 
 class Contact extends StatefulWidget {
   const Contact({Key? key}) : super(key: key);
@@ -11,6 +13,20 @@ class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact us", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+        leading: BackButton(
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => MyApp()));
+          },
+        ),
+        //title: Text("Contact us"),
+      ),
     );
   }
 }
