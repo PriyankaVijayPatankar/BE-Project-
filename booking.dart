011341639d0a81509b724d8fd4c51804f610fe3f
Priyanka Patankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2023-02-22 21:25:15 +0530

    by P

--- booking.dart
+++ booking.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:myapp/homepage.dart';
-import 'package:myapp/main.dart';
+import 'booknow.dart';
 
 class Booking extends StatefulWidget {
   const Booking({Key? key}) : super(key: key);
@@ -12,52 +12,137 @@ class Booking extends StatefulWidget {
 }
 
 class _BookingState extends State<Booking> {
-  List<String> famcottage = [
-    "Hello World",
-    "This is FlutterCampus",
-    "Learn App Building.",
-    "Flutter is the Best"
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Booking", style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        elevation: 0,
-        leading: BackButton(
-          color: Colors.black,
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => HomePage()));
-          },
-        ),
-      ),
-      body: Column(
-        children:
-            // Text("Family Cottage"),
-            // Image.asset('assets/images/sindhu.jpeg'),
-            famcottage.map((strone) {
-          return Row(children: [
-            //Text("Family Cottage"),
-            //Image.asset('assets/images/sindhu.jpeg'),
-            Text(
-              "\u2022",
-              style: TextStyle(fontSize: 30),
-            ), //bullet text
-            SizedBox(
-              width: 10,
-            ), //space between bullet and text
-            Expanded(
-              child: Text(
-                strone,
-                style: TextStyle(fontSize: 30),
-              ), //text
-            )
-          ]);
-        }).toList(),
-      ),
-    );
+        appBar: bookNow(context),
+        body: Builder(
+            builder: (context) => SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Family Cottage',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
+                        textAlign: TextAlign.start,
+                      ),
+                      // make the image size large, occupying the whole width of screen, and atleast 40% of vertical screen
+                      Image.asset(
+                        'images/sindhu.jpg',
+                        //scale: 0.8,
+                        fit: BoxFit.contain,
+                        width: double.infinity,
+                      ),
+
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                      Text(
+                          '4 Beds\nAttached Washroom\nDaily Housekeeping\nAC/Non-AC\nRoom Service\nComplimentary Mineral Water\nCoffee/Tea Services\n24/7 Hot bathing water\nExtra bed if required'),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: ButtonTheme(
+                                //minWidth: double.infinity,
+                                height: 60,
+                                child: ElevatedButton(
+                                  //style: ElevatedButton.styleFrom(
+                                  // backgroundColor: Colors.black,
+                                  // foregroundColor: Colors.white,
+                                  //),
+                                  child: Text('Book Now     >',
+                                      style: TextStyle(fontSize: 15)),
+                                  //color: Colors.black,
+                                  //textColor: Colors.white,
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) => BookNow()));
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'Couple Cottage',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
+                        //textAlign: TextAlign.left,
+                      ),
+                      // make the image size large, occupying the whole width of screen, and atleast 40% of vertical screen
+                      Image.asset(
+                        'images/sindhu.jpg',
+                        //scale: 0.8,
+                        fit: BoxFit.contain,
+                        width: double.infinity,
+                      ),
+
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                      Text(
+                          '2 Beds\nAttached Washroom\nDaily Housekeeping\nAC/Non-AC\nRoom Service\nComplimentary Mineral Water\nCoffee/Tea Services\n24/7 Hot bathing water\nExtra bed if required'),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: ButtonTheme(
+                                //minWidth: double.infinity,
+                                height: 60,
+
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      // backgroundColor: Colors.black,
+                                      // foregroundColor: Colors.white,
+                                      // padding: EdgeInsets.all(20.0),
+                                      ),
+                                  child: Text('Book Now      >',
+                                      style: TextStyle(
+                                          //fontWeight: FontWeight.bold,
+                                          fontSize: 15)),
+                                  //color: Colors.black,
+                                  //textColor: Colors.white,
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) => BookNow()));
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )));
   }
+}
+
+PreferredSizeWidget bookNow(BuildContext context) {
+  return AppBar(
+    title: const Text("Booking", style: TextStyle(color: Colors.black)),
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    elevation: 0,
+    leading: BackButton(
+      color: Colors.black,
+      onPressed: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => HomePage()));
+      },
+    ),
+  );
 }
