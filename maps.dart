811298ad8da47e5a54c9b201b4236f2e2abd2733
Priyanka Patankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2023-02-22 21:34:48 +0530

    by P

--- maps.dart
+++ maps.dart
@@ -1,6 +1,7 @@
+//import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:mymobileapp/main.dart';
-import 'homepage.dart';
+import 'package:myapp/main.dart';
+//import 'package:url_launcher/url_launcher.dart';
 
 class Maps extends StatefulWidget {
   const Maps({Key? key}) : super(key: key);
@@ -26,13 +27,43 @@ class _MapsState extends State<Maps> {
             },
           ),
         ),
-        body: Container(
-          height: 400,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/maps.jpg"), fit: BoxFit.cover),
-          ),
-        ));
+        body: Builder(
+            builder: (context) => SingleChildScrollView(
+                    child: Column(
+                  children: [
+                    Container(
+                      height: 400,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("images/maps.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    // Text.rich(TextSpan(
+                    //     style: TextStyle(
+                    //         color: Colors.blue,
+                    //         decoration: TextDecoration.underline),
+                    //     text:
+                    //         "Visit Google maps to reach Amrapali Gramsahavas from your current location:",
+                    //     recognizer: TapGestureRecognizer()
+                    //       ..onTap = () async {
+                    //on tap code here, you can navigate to other page or URL
+                    //   String url =
+                    //       "https://goo.gl/maps/eyMet2iHStP2eyWR7";
+                    //   var urllaunchable = await canLaunch(
+                    //       url); //canLaunch is from url_launcher package
+                    //   if (urllaunchable) {
+                    //     await launch(
+                    //         url); //launch is from url_launcher package to launch URL
+                    //   } else {
+                    //     Text("URL can't be launched.");
+                    //   }
+                    // }))
+                  ],
+                ))));
   }
 }
